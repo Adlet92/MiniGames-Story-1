facc4004c08587eaf80707b424378f306cf55772
root <root@LAPTOP-T771OUC9.>
2026-09-25 18:28:58 +0500

    feat: add footer from story-1

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import type { AuthDialog } from '../components/dialogs/auth-dialog';
 import { createAuthDialog } from '../components/dialogs/auth-dialog';
+import { createFooter } from '../components/footer/footer';
 import type { HeaderOptions } from '../components/header/header';
 import { createHeader } from '../components/header/header';
 import { createHomePage } from '../components/home/home-page';
@@ -9,5 +10,5 @@ const authDialog: AuthDialog = createAuthDialog();
 const options: HeaderOptions = { onAuth: authDialog.open };
 const app: HTMLDivElement = document.createElement('div');
 app.id = 'app';
-app.append(createHeader(options), createHomePage(), authDialog.element);
+app.append(createHeader(options), createHomePage(), createFooter(), authDialog.element);
 document.body.append(app);
